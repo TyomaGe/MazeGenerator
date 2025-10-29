9193c45a63449aecbb1a19e12db2ad0643a10310
Tyoma Gorbunov <bivaetctoskazat123@gmail.com>
2025-10-29 09:37:16 +0500

    feat: Изменен выбор клеток на удаление, убраны методы создания боковых проходов

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from maze import MazeGenerator
 if __name__ == "__main__":
     seed = random()
     maze_generator = MazeGenerator()
-    maze = maze_generator.generate(50, 50, finish=(49, 49), seed=seed, shortcut_coef=0.05)
-    drawer = MazeDrawer(maze, cell_size=7)
+    maze = maze_generator.generate(10, 10, seed=seed, shortcut_coef=0.0)
+    drawer = MazeDrawer(maze, cell_size=30)
     drawer.draw()
--- maze.py
+++ maze.py
@@ -69,33 +69,21 @@ class MazeGenerator:
         self.__maze.empty((rmap(fx) + dx, rmap(fy) + dy))
         self.__maze.empty(self.__rpmap(s_point))
 
-    def __delete_border_wall(self, point):
-        x, y = point
-        rx, ry = self.__rpmap(point)
-        if y == 0:
-            self.__maze.empty((rx, ry - 1))
-        elif y == self.__lheight - 1:
-            self.__maze.empty((rx, ry + 1))
-        elif x == 0:
-            self.__maze.empty((rx - 1, ry))
-        elif x == self.__lwidth - 1:
-            self.__maze.empty((rx + 1, ry))
-
     def __set_start(self, start):
         self.__maze.empty(self.__rpmap(start))
-        self.__delete_border_wall(start)
 
-    def __set_finish(self, finish):
-        self.__delete_border_wall(finish)
-
-    def __is_logically_inside(self, point):
+    def __is_inside(self, point):
         x, y = point
         return 0 <= x < self.__lwidth and 0 <= y < self.__lheight
+
+    def __is_connectable(self, neighbour):
+        rneighbour = self.__rpmap(neighbour)
+        return self.__is_inside(neighbour) and self.__maze.is_wall(rneighbour)
 
     def __get_neighbour(self, current, direction):
         return current[0] + direction[0], current[1] + direction[1]
 
-    def generate(self, lwidth, lheight, start=(0, 0), finish=(0, 0), seed=0, shortcut_coef=0.0):
+    def generate(self, lwidth, lheight, start=(0, 0), seed=0, shortcut_coef=0.0):
         self.__lwidth = lwidth
         self.__lheight = lheight
         self.__rwidth = self.__rmap(lwidth)
@@ -110,40 +98,40 @@ class MazeGenerator:
             moved = False
             for direction in self.__DIRECTIONS:
                 neighbor = self.__get_neighbour(current, direction)
-                if self.__is_logically_inside(neighbor):
-                    if self.__maze.is_wall(self.__rpmap(neighbor)):
-                        self.__connect(current, neighbor)
-                        stack.append(neighbor)
-                        moved = True
-                        break
+                if self.__is_connectable(neighbor):
+                    self.__connect(current, neighbor)
+                    stack.append(neighbor)
+                    moved = True
+                    break
             if not moved:
                 stack.pop()
-        self.__set_finish(finish)
         self.__add_shortcuts(shortcut_coef)
         return self.__maze.get_maze()
 
     def __add_shortcuts(self, shortcut_coef):
-        if shortcut_coef != 0.0:
-            is_deletable = self.__is_deletable
-            is_wall = self.__maze.is_wall
-            check_fn = is_deletable if shortcut_coef < 0.9 else is_wall
-            candidates = []
-            for y in range(1, self.__maze.height - 1):
-                for x in range(1, self.__maze.width - 1):
-                    point = (x, y)
-                    if check_fn(point):
-                        candidates.append(point)
-            random.shuffle(candidates)
-            shortcuts_target = int(len(candidates) * shortcut_coef)
-            for i in range(min(shortcuts_target, len(candidates))):
-                self.__maze.empty(candidates[i])
+        if shortcut_coef <= 0.0:
+            return
+        is_deletable = self.__is_deletable
+        is_wall = self.__maze.is_wall
+        check_fn = is_deletable if shortcut_coef < 0.75 else is_wall
+        candidates = []
+        for y in range(1, self.__maze.height - 1):
+            for x in range(1, self.__maze.width - 1):
+                point = (x, y)
+                if check_fn(point):
+                    candidates.append(point)
+        random.shuffle(candidates)
+        shortcuts_target = int(len(candidates) * shortcut_coef)
+        for i in range(min(shortcuts_target, len(candidates))):
+            self.__maze.empty(candidates[i])
 
     def __is_deletable(self, point):
-        x, y = point
         if self.__maze.is_empty(point):
             return False
-        left = self.__maze.is_empty((x - 1, y))
-        right = self.__maze.is_empty((x + 1, y))
-        up = self.__maze.is_empty((x, y - 1))
-        down = self.__maze.is_empty((x, y + 1))
-        return (left and right) or (up and down)
+        x, y = point
+        left = self.__maze.is_wall((x - 1, y))
+        right = self.__maze.is_wall((x + 1, y))
+        up = self.__maze.is_wall((x, y - 1))
+        down = self.__maze.is_wall((x, y + 1))
+        is_corner = (up or down) and (right or left)
+        return not is_corner
